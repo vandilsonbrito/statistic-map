dc161098c3ea29a950fe79a96a2c69dc3e61c96d
vandilsonbrito <vandilson25@outlook.com>
2024-08-19 22:12:34 -0300

    Feat: add map layer that possibilite see different color for selected country

--- components/map/Map.tsx
+++ components/map/Map.tsx
@@ -1,13 +1,53 @@
 'use client'
-import { MapContainer, TileLayer, useMapEvents } from 'react-leaflet';
+import { MapContainer, TileLayer, useMapEvents, GeoJSON} from 'react-leaflet';
 import { useAtom } from 'jotai';
-import { latAtom, lngAtom, isMapClickedAtom } from '@/utils/stores/atoms';
+import { latAtom, lngAtom, isMapClickedAtom, countryName } from '@/utils/stores/atoms';
 import 'leaflet/dist/leaflet.css'; 
+import { useEffect, useState } from 'react';
+import { GeoJSONFeature, FeatureProperties } from '../../utils/types/types';
+import { Layer, PathOptions } from 'leaflet';
+
 
 function Map() {
     const [lat, setLat] = useAtom(latAtom);
     const [lng, setLng] = useAtom(lngAtom);
     const [mapClicked, setMapClicked] = useAtom(isMapClickedAtom);
+    const [geojsonData, setGeojsonData] = useState<any>(null);
+    const [selectedCountry, setSelectedCountry] = useAtom(countryName);
+
+    useEffect(() => {
+        const fetchGeojson = async () => {
+            const response = await fetch('/globe.geo.json');
+            const data = await response.json();
+            console.log("DADOS", data)
+            setGeojsonData(data);
+        }
+        fetchGeojson();
+
+    }, [])
+
+    const onEachFeature = (feature: GeoJSONFeature, layer: Layer) => {
+        layer.on({
+          click: () => {
+            setSelectedCountry(feature.properties.name);
+          }
+        });
+      };
+
+      
+    const style = (feature?: GeoJSONFeature): PathOptions => {
+        if (!feature) {
+            return {};
+        }
+        return {
+          fillColor: feature.properties.name === selectedCountry ? 'blue' : 'transparent',
+          weight: 2,
+          opacity: 1,
+          color: 'white',
+          dashArray: '3',
+          fillOpacity: 0.7
+        };
+    };
 
     function MapEvents() {
         const map = useMapEvents({
@@ -22,12 +62,19 @@ function Map() {
     }
 
     return (
-        <MapContainer center={[21.505, -0.09]} zoom={2} scrollWheelZoom={true} className="w-full h-[400px] rounded-lg shadow-lg">
+        <MapContainer center={[21.505, -0.09]} zoom={2} scrollWheelZoom={true} className="w-full h-[350px] rounded-lg shadow-lg">
             <MapEvents />
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+            {geojsonData && (
+                <GeoJSON
+                data={geojsonData}
+                style={style}
+                onEachFeature={onEachFeature}
+                />
+            )}
         </MapContainer>
     );
 }
