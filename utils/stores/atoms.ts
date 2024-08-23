913eab1c37aa329bb911e814f3b326a9a5ed9a96
vandilsonbrito <vandilson25@outlook.com>
2024-08-22 22:06:20 -0300

    update: create covidDeathsDataFetched atom and update casesData  and covidCasesDataFetched

--- utils/stores/atoms.ts
+++ utils/stores/atoms.ts
@@ -1,11 +1,12 @@
 import { atom } from 'jotai';
-import { Cases, DataFetched } from '../types/types';
+import { Cases, CaseData, CasesDataFetched, DeathsDataFetched } from '../types/types';
 
 export const latAtom = atom<string>('');
 export const lngAtom = atom<string>('');
 export const isMapClickedAtom = atom<boolean>(false);
 export const countryName = atom<string>('Brazil');
-export const covidDataFetched = atom<DataFetched[]>([]);
-export const casesData = atom<Cases>({});
+export const covidCasesDataFetched = atom<CasesDataFetched[]>([]);
+export const covidDeathsDataFetched = atom<DeathsDataFetched[]>([]);
+export const casesData = atom<Cases[]>([]);
 export const latestDay = atom<string>('');
 export const dataLatestDay = atom<string>('');
